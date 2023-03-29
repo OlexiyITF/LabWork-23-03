d49c05c6aa5513c629b3540d2f74ab8fcd96ee44
olexiyITF <olexi@DESKTOP-V0FS0P2>
2023-03-29 09:53:46 +0300

    Task 3 complited

--- LabWork-23-03/main.cpp
+++ LabWork-23-03/main.cpp
@@ -1,12 +1,24 @@
 #include <iostream>
-using namespace std;
+#include "points.h"
+#include <iomanip>
+
+Cpoint <int> read_point();
 
 int main()
 {
+	/*Введіть з клавіатури точки A, B, C (координати задані цілим числами),
+	* що є вершинами деякого трикутрина. Обчисліть і надрукуйте
+	* периметр і площу цього трикутника	*/
+	CPoint <int> A = read_point();
+	CPoint <int> B = read_point();
+	CPoint <int> C = read_point();
+	std::cout << " A=" << std::showpoint << A << '\n' << "B =" << B << '\n';
+	int a = A.distance(B);
+	int a = A.distance(C);
+	int a = A.distance(A);
+	int P = a + b + c;
+	double p = P * 0.5;
+	double S = sqrt(p * (p - a) * (p - b * (p - c)));
+	std::cout << "P = "  << 
 
-
-
-
-
-	return 0;
 }
--- LabWork-23-03/points.h
+++ LabWork-23-03/points.h
@@ -6,7 +6,7 @@ class Cpoint
 {
 	TNum x, y;
 public:
-	CPoint(TNum a, TNum b) : x(a), y(b) {}
+	CPoint(TNum a, TNum b) :x(a), y(b) {}
 	CPoint() : x(0), y(0) {} //TNum(0) ???
 	TNum get_x() const { return x; }
 	TNum get_y() const { return y; }
@@ -24,4 +24,17 @@ public:
 	{
 		return CPoint - (this->x + p.x, this->y + p.y);
 	}
+	TNum distance(const CPoint& p) const;
 };
+
+template <typename TNum>
+inline TNum Cpoint<TNum>::distance(const CPoint& p) const
+{
+	return TNum();
+}
+
+template<typename TNum>
+std::ostream& operator<<(std::ostream& os, CPoint<TNum> A)
+{
+	A.print_on(os); return os;
+}
